2a18e66a57dd46d968435758c9c9c909e83b720f
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-21 09:24:00 +0200

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -13,14 +13,21 @@ spawn(function()
         wait(4)
     end
 end)
-getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-    if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-        local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
-        for i,v in next, Gay.data do
-            if v.playing < 2 then
-                TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
-                break
-            end
-        end
+spawn(function()
+    while wait() do
+        pcall(function()
+            getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
+                if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+                    local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+                    for i,v in next, Gay.data do
+                        if v.playing < 2 then
+                            TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
+                            break
+                        end
+                    end
+                end
+            end)
+        end)
+        wait(4)
     end
 end)
