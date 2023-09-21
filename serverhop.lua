eae3cd5de19a2534766d54c6e52f4404a9a3c2da
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-21 09:28:09 +0200

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,8 +1,11 @@
+
+
 spawn(function()
     while wait() do
         wait(math.random(1200))
         pcall(function()
             local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+            local Player = Players.LocalPlayer
             for i,v in next, Gay.data do
                 if v.playing < 2 then
                     TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
@@ -19,6 +22,7 @@ spawn(function()
             getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
                 if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
                     local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+                    local Player = Players.LocalPlayer
                     for i,v in next, Gay.data do
                         if v.playing < 2 then
                             TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
