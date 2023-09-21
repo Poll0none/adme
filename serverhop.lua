ddb726ce57efddc3bd884fc005346b28fa98af11
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-21 23:14:05 +0200

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,17 +1,22 @@
+local ServerHopper = function()
+    local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+    local Player = Players.LocalPlayer
 
+    for i,v in next, Gay.data do
+        if v.playing < 2 then
+            TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
+            break
+        end
+    end
+end
 
 spawn(function()
     while wait() do
-        wait(math.random(1200))
+        wait(1200)
         pcall(function()
-            local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
-            local Player = Players.LocalPlayer
-            for i,v in next, Gay.data do
-                if v.playing < 2 then
-                    TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
-                    break
-                end
-            end
+            
+            ServerHopper()
+            
         end)
         wait(4)
     end
@@ -21,14 +26,20 @@ spawn(function()
         pcall(function()
             getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
                 if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-                    local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
-                    local Player = Players.LocalPlayer
-                    for i,v in next, Gay.data do
-                        if v.playing < 2 then
-                            TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
-                            break
-                        end
-                    end
+
+                    -- Create an array of delay values in seconds
+                    local delayValues = {1, 10, 20, 30, 40, 50}
+                    
+                    -- Select a random delay value from the array
+                    local randomIndex = math.random(1, #delayValues)
+                    local selectedDelay = delayValues[randomIndex]
+                    
+                    -- Wait for the selected delay
+                    wait(selectedDelay)
+                    
+                    -- Now you've waited for a random delay based on the values in the array
+                    ServerHopper()
+
                 end
             end)
         end)
