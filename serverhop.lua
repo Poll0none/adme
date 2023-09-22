f7ddfc777787b83ff32a8c4ebdcf03bf6e83ffff
Poll0none <126181078+Poll0none@users.noreply.github.com>
2023-09-22 16:27:13 +0200

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,23 +1,12 @@
---Do _G.ServerHopWait = 1200 before the loadstring
-if _G.ServerHopWait == nil then
-    _G.ServerHopWait = 1200
-end
+--TRIED TO MAKE IT READ A VARIABLE ACROSS MULTI INSTANCES, proved to be impossible, have to look into HTTP GET and POST
 
-if _G.ServerHopDelay == nil then
-    _G.serverHopDelay = 20
-end
-
-local HopWait = _G.ServerHopWait
-local HopDelay = _G.ServerHopDelay
-_G.ServerHopCalc = HopWait + HopDelay
-
-local ServerHopper = function()
-    local Players = game:GetService("Players")
-    local Player = Players.LocalPlayer
+local ServerHopper = function(time)
+    wait(time)
     local HttpService = game:GetService("HttpService")
+    local Players = game:GetService("Players")
     local TeleportService = game:GetService("TeleportService")
     local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
-
+    local Player = Players.LocalPlayer
     for i,v in next, Gay.data do
         if v.playing < 2 then
             TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
@@ -25,36 +14,26 @@ local ServerHopper = function()
         end
     end
 end
+
 spawn(function()
-    while wait() do
-        wait(_G.ServerHopCalc)
-        pcall(function()
-            
-            ServerHopper()
-            
-        end)
-        wait(4)
-    end
+    ServerHopper(120)
 end)
+
 spawn(function()
     while wait() do
         pcall(function()
             getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-                            
-                --LINE ABOVE WORKS but sometimes doesn´t detect??
-                --if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-                            
-                if child.Name == 'ErrorPrompt' then
+                if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
 
                     -- Create an array of delay values in seconds
-                    local delayValues = {10, 20, 30, 40, 50, 60, 70}
+                    local delayValues = {1, 10, 20, 30, 40, 50}
                     
                     -- Select a random delay value from the array
                     local randomIndex = math.random(1, #delayValues)
                     local selectedDelay = delayValues[randomIndex]
                     
                     -- Wait for the selected delay
-                    wait(_G.serverHopDelay)
+                    wait()
                     
                     -- Now you've waited for a random delay based on the values in the array
                     ServerHopper()
